36023923c7f5321276f61805792bb81f90c62f89
yuichirou.ishikawa <yuichirou.ishikawa@mixi.co.jp>
2017-11-28 17:50:55 +0900

    problem 4

--- utils.go
+++ utils.go
@@ -161,3 +161,21 @@ func NewPrimeGenerator() *PrimeGenerator {
 
 	return &p
 }
+
+func isPalindrome(s string) bool {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		if r[i] != r[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func pow10(n int) int {
+	m := 1
+	for i := 0; i < n; i++ {
+		m *= 10
+	}
+	return m
+}
--- utils_test.go
+++ utils_test.go
@@ -30,3 +30,36 @@ func Benchmark_PrimeGenerator(b *testing.B) {
 	}
 	fmt.Println(PrimeGenerator.Size())
 }
+
+func Test_isPalrindrome(t *testing.T) {
+	cases := []struct {
+		Input    string
+		Expected bool
+	}{
+		{"abcde", false},
+		{"abcba", true},
+		{"abccba", true},
+	}
+	for _, tc := range cases {
+		if actual := isPalindrome(tc.Input); actual != tc.Expected {
+			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
+		}
+	}
+}
+
+func Test_pow10(t *testing.T) {
+	cases := []struct {
+		Input    int
+		Expected int
+	}{
+		{0, 1},
+		{1, 10},
+		{2, 100},
+		{3, 1000},
+	}
+	for _, tc := range cases {
+		if actual := pow10(tc.Input); actual != tc.Expected {
+			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
+		}
+	}
+}
