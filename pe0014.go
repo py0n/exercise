c7fd90f5063ef5d4beb62d03209497f15c1533b2
ISHIKAWA YUICHIRO <py.o.n.13+git@gmail.com>
2017-12-01 13:10:11 +0900

    別の方式を試した (失敗)

--- pe0014.go
+++ pe0014.go
@@ -22,3 +22,34 @@ func countCollatzSequenceLength(n int) int {
 	}
 	return c + 1
 }
+
+/*
+計算済の数字をマップで記録したら寧ろ遅くなった。
+メモリ確保の為。
+Benchmark_PE0014-2         10000           2291253 ns/op               0 B/op          0 allocs/op
+Benchmark_PE0014_2-2       10000          14047469 ns/op          854232 B/op       1177 allocs/op
+PASS
+ok      github.com/py0n/project_euler   210.552s
+*/
+func PE0014_2(n int) int {
+	trush := map[int]bool{}
+	maxLength := 0
+	for i := 1; i < n; i++ {
+		if trush[i] {
+			continue
+		}
+		c := 0
+		for m := i; m > 1; c++ {
+			if m%2 == 0 {
+				m /= 2
+			} else {
+				m = 3*m + 1
+			}
+			trush[m] = true
+		}
+		if c+1 > maxLength {
+			maxLength = c + 1
+		}
+	}
+	return maxLength
+}
--- pe0014_test.go
+++ pe0014_test.go
@@ -16,6 +16,36 @@ func Test_PE0014(t *testing.T) {
 	}
 }
 
+func Benchmark_PE0014(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		PE0014(b.N)
+	}
+}
+
+/*
+func Test_PE0014_2(t *testing.T) {
+	cases := []struct {
+		Input    int
+		Expected int
+	}{
+		{10000000, 686},
+	}
+	for _, tc := range cases {
+		if actual := PE0014_2(tc.Input); actual != tc.Expected {
+			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
+		}
+	}
+}
+*/
+
+func Benchmark_PE0014_2(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		PE0014_2(b.N)
+	}
+}
+
 func Test_countCollatzSequenceLength(t *testing.T) {
 	cases := []struct {
 		Input    int
