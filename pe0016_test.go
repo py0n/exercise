9df15d5a60c0e2596f5692f86ced3562490bceff
Pyon <py0n@users.noreply.github.com>
2017-12-01 16:10:07 +0900

    problem 16

---
+++ pe0016_test.go
@@ -0,0 +1,18 @@
+package project_euler
+
+import "testing"
+
+func Test_PE0016(t *testing.T) {
+	cases := []struct {
+		Input    int
+		Expected int
+	}{
+		{15, 26},
+		{1000, 1366},
+	}
+	for _, tc := range cases {
+		if actual := PE0016(tc.Input); actual != tc.Expected {
+			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
+		}
+	}
+}
