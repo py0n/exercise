27e4f8ff133c6cc558a425bc3cbf0f309531c066
ISHIKAWA YUICHIRO <p.y.o.n.1.3+git@gmail.com>
2017-12-02 23:54:14 +0900

    probllem 15: 動的計畫法やメモ化を使用した凾數を追加

--- pe0015.go
+++ pe0015.go
@@ -5,35 +5,87 @@ import (
 )
 
 // https://projecteuler.net/problem=15
+//
+// Benchmark_PE0015-4                        300000              9816 ns/op            3440 B/op         86 allocs/op
+// Benchmark_PE0015Dp-4                      100000             12500 ns/op            4096 B/op          1 allocs/op
+// Benchmark_PE0015Memoization-4             300000              5701 ns/op            4096 B/op          1 allocs/op
+// PASS
+// ok      github.com/py0n/project_euler   6.166s
 
-/*
-	+-+-+-+
-	| | | |
-	+-+-+-+
-	| | | |
-	+-+-+-+
-	| | | |
-	+-+-+-+
-
-	|  | 1| 2| 3| 4| 5|
-	| 1| 2| 3| 4| 5| 6|
-	| 2| 3| 6|10|15|
-	| 3| 4|10|20|  |
-	| 4| 5|15|  |  |
-	| 5| 6|
-
-	n * m -> (n+m)Cn
-*/
-func PE0015(n int64) int64 {
+// PE0015 組み合はせを使用して計算 (bigも使用)
+//
+// +-+-+-+
+// | | | |
+// +-+-+-+
+// | | | |
+// +-+-+-+
+// | | | |
+// +-+-+-+
+//
+// |  | 1| 2| 3| 4| 5|
+// | 1| 2| 3| 4| 5| 6|
+// | 2| 3| 6|10|15|
+// | 3| 4|10|20|  |
+// | 4| 5|15|  |  |
+// | 5| 6|
+//
+// n * m -> (n+m)Cn
+func PE0015(n int) int64 {
 	numerator := big.NewInt(1) // 分子
-	for i := int64(0); i < n; i++ {
+	for i := 0; i < n; i++ {
 		numerator.Mul(numerator, big.NewInt(int64(2*n-i)))
 	}
 	denominator := big.NewInt(1) //分母
-	for i := int64(0); i < n; i++ {
+	for i := 0; i < n; i++ {
 		denominator.Mul(denominator, big.NewInt(int64(n-i)))
 	}
 	r := big.NewInt(0)
 	r.Quo(numerator, denominator)
 	return r.Int64()
+}
+
+// PE0015Dp DPを使用して計算
+//
+// + - 1 - 1 - 1
+// |   |   |   |
+// 1 - 2 - 3 - 4
+// |   |   |   |
+// 1 - 3 - 6 -10
+// |   |   |   |
+// 1 - 4 -10 -20
+func PE0015Dp(n int) int64 {
+	m := n + 1 // 一辺の點の數
+	grid := make([]int64, m*m)
+	grid[0] = 1 // 始點
+	for i := 1; i < m*m; i++ {
+		// 左の點が存在するなら、其處迄の經路數を加算
+		if j := i % m; j > 0 {
+			grid[i] += grid[i-1]
+		}
+		// 上の點が存在するなら、其處までの經路數を加算
+		if j := i - m; j >= 0 {
+			grid[i] += grid[i-m]
+		}
+	}
+	return grid[m*m-1]
+}
+
+// PE0015Memoization メモ化を利用して計算 (再歸を試したが計算が終了しなかつた)
+func PE0015Memoization(n int) int64 {
+	memo := make([]int64, (n+1)*(n+1))
+	return routeNumber(n, n, n+1, memo)
+}
+
+func routeNumber(i, j, n int, memo []int64) int64 {
+	if i < 0 || j < 0 {
+		return 0
+	} else if i == 0 && j == 0 {
+		return 1
+	} else if memo[j*n+i] > 0 {
+		return memo[j*n+i]
+	} else if memo[i*n+j] > 0 {
+		return memo[i*n+j]
+	}
+	memo[j*n+i] = routeNumber(i-1, j, n, memo) + routeNumber(i, j-1, n, memo)
+	return memo[j*n+i]
 }
--- pe0015_test.go
+++ pe0015_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func Test_PE0015(t *testing.T) {
 	cases := []struct {
-		Input    int64
+		Input    int
 		Expected int64
 	}{
 		{2, 6},
@@ -15,5 +15,55 @@ func Test_PE0015(t *testing.T) {
 		if actual := PE0015(tc.Input); actual != tc.Expected {
 			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
 		}
+	}
+}
+
+func Test_PE0015Dp(t *testing.T) {
+	cases := []struct {
+		Input    int
+		Expected int64
+	}{
+		{2, 6},
+		{3, 20},
+		{20, 137846528820},
+	}
+	for _, tc := range cases {
+		if actual := PE0015Dp(tc.Input); actual != tc.Expected {
+			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
+		}
+	}
+}
+
+func Test_PE0015Memoization(t *testing.T) {
+	cases := []struct {
+		Input    int
+		Expected int64
+	}{
+		{2, 6},
+		{3, 20},
+		{20, 137846528820},
+	}
+	for _, tc := range cases {
+		if actual := PE0015Memoization(tc.Input); actual != tc.Expected {
+			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
+		}
+	}
+}
+
+func Benchmark_PE0015(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PE0015(20)
+	}
+}
+
+func Benchmark_PE0015Dp(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PE0015Dp(20)
+	}
+}
+
+func Benchmark_PE0015Memoization(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PE0015Memoization(20)
 	}
 }
