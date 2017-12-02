ddee2b5270fecd5151d468787dcdc5e3e310dfca
Pyon <py0n@users.noreply.github.com>
2017-12-03 04:12:43 +0900

    problem18をメモ化で解く

--- pe0018.go
+++ pe0018.go
@@ -1,23 +1,40 @@
 package project_euler
 
-var pe0018Data = [][]string{
-	[]string{"75"},
-	[]string{"95", "64"},
-	[]string{"17", "47", "82"},
-	[]string{"18", "35", "87", "10"},
-	[]string{"20", "04", "82", "47", "65"},
-	[]string{"19", "01", "23", "75", "03", "34"},
-	[]string{"88", "02", "77", "73", "07", "63", "67"},
-	[]string{"99", "65", "04", "28", "06", "16", "70", "92"},
-	[]string{"41", "41", "26", "56", "83", "40", "80", "70", "33"},
-	[]string{"41", "48", "72", "33", "47", "32", "37", "16", "94", "29"},
-	[]string{"53", "71", "44", "65", "25", "43", "91", "52", "97", "51", "14"},
-	[]string{"70", "11", "33", "28", "77", "73", "17", "78", "39", "68", "17", "57"},
-	[]string{"91", "71", "52", "38", "17", "14", "91", "43", "58", "50", "27", "29", "48"},
-	[]string{"63", "66", "04", "68", "89", "53", "67", "30", "73", "16", "69", "87", "40", "31"},
-	[]string{"04", "62", "98", "27", "23", "09", "70", "98", "73", "93", "38", "53", "60", "04", "23"},
-}
+import (
+	"strconv"
+)
 
-func PE0018() int {
-	return 0
+// https://projecteuler.net/problem=18
+
+// PE0018Memoization Problem 18をメモ化を使用して計算
+func PE0018Memoization(size int, data []string) int {
+
+	var routeSum func(int, int, int, []string, []int) int
+	routeSum = func(i, j, size int, data []string, memo []int) int {
+		if i < 0 || j < 0 {
+			return 0
+		} else if i > j {
+			return 0
+		}
+		p := j*(j+1)/2 + i
+		m, _ := strconv.Atoi(data[p])
+		if memo[p] > 0 {
+			return memo[p]
+		}
+		if routeSum(i-1, j-1, size, data, memo) > routeSum(i, j-1, size, data, memo) {
+			memo[p] = m + routeSum(i-1, j-1, size, data, memo)
+		} else {
+			memo[p] = m + routeSum(i, j-1, size, data, memo)
+		}
+		return memo[p]
+	}
+
+	memo := make([]int, size*(size+1)/2)
+	nMax := 0
+	for i := 0; i < size; i++ {
+		if routeSum(i, size-1, size, data, memo) > nMax {
+			nMax = routeSum(i, size-1, size, data, memo)
+		}
+	}
+	return nMax
 }
