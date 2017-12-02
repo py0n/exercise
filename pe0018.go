9e0a47cc5d3c1b1ca4cae81596c98dba3a979ac6
Pyon <py0n@users.noreply.github.com>
2017-12-03 04:19:37 +0900

    Closure化

--- pe0018.go
+++ pe0018.go
@@ -8,32 +8,30 @@ import (
 
 // PE0018Memoization Problem 18をメモ化を使用して計算
 func PE0018Memoization(size int, data []string) int {
+	memo := make([]int, size*(size+1)/2) // メモ
 
-	var routeSum func(int, int, int, []string, []int) int
-	routeSum = func(i, j, size int, data []string, memo []int) int {
-		if i < 0 || j < 0 {
-			return 0
-		} else if i > j {
+	var routeSum func(int, int) int
+	routeSum = func(i, j int) int {
+		if i < 0 || j < 0 || i > j {
 			return 0
 		}
 		p := j*(j+1)/2 + i
-		m, _ := strconv.Atoi(data[p])
 		if memo[p] > 0 {
 			return memo[p]
 		}
-		if routeSum(i-1, j-1, size, data, memo) > routeSum(i, j-1, size, data, memo) {
-			memo[p] = m + routeSum(i-1, j-1, size, data, memo)
+		m, _ := strconv.Atoi(data[p])
+		if routeSum(i-1, j-1) > routeSum(i, j-1) {
+			memo[p] = m + routeSum(i-1, j-1)
 		} else {
-			memo[p] = m + routeSum(i, j-1, size, data, memo)
+			memo[p] = m + routeSum(i, j-1)
 		}
 		return memo[p]
 	}
 
-	memo := make([]int, size*(size+1)/2)
 	nMax := 0
 	for i := 0; i < size; i++ {
-		if routeSum(i, size-1, size, data, memo) > nMax {
-			nMax = routeSum(i, size-1, size, data, memo)
+		if routeSum(i, size-1) > nMax {
+			nMax = routeSum(i, size-1)
 		}
 	}
 	return nMax
