930860f96be7b3c0ca966f386da6e27b0c535785
Pyon <py0n@users.noreply.github.com>
2017-12-04 17:13:06 +0900

    skelton

---
+++ pe0020.go
@@ -0,0 +1,6 @@
+package project_euler
+
+// PE0020 階乗の各桁の和を計算
+func PE0020(n int) int {
+	return 0
+}
---
+++ pe0020_test.go
@@ -0,0 +1,18 @@
+package project_euler
+
+import "testing"
+
+var pe0020Cases = []struct {
+	Input    int
+	Expected int
+}{
+	{10, 27},
+}
+
+func Test_PE0020(t *testing.T) {
+	for _, tc := range pe0020Cases {
+		if actual := PE0020(tc.Input); actual != tc.Expected {
+			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
+		}
+	}
+}
