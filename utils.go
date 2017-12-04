93d830cab060119cfbdac7268b60f5c434477b63
Pyon <py0n@users.noreply.github.com>
2017-12-04 23:09:56 +0900

    Zellerの公式凾數を公開凾數に

--- utils.go
+++ utils.go
@@ -222,17 +222,26 @@ func Pow10(n int) int {
 	return Pow(10, n)
 }
 
-func multiplyDigits(n int) int {
-	product := 1
-	for n > 0 {
-		product *= n % 10
-		n = n / 10
-	}
-	return product
-}
+// DayOfWeek 曜日を表す型
+type DayOfWeek int
 
-// zellerWeekday ツェラーの公式
-func zellerWeekday(y, m, d int) int {
+// 曜日
+const (
+	None      DayOfWeek = iota // 該當無し
+	Monday                     // 月曜日
+	Tuesday                    // 火曜日
+	Wednesday                  // 水曜日
+	Thursday                   // 木曜日
+	Friday                     // 金曜日
+	Saturday                   // 土曜日
+	Sunday                     // 日曜日
+)
+
+// ZellerWeekday ツェラーの公式
+func ZellerWeekday(y, m, d int) DayOfWeek {
+	if y < 1582 || (y == 1582 && m < 10) || (y == 1582 && m == 10 && d < 15) {
+		return None
+	}
 	y_, m_ := y, m
 	if m < 3 {
 		y_, m_ = y-1, m+12
@@ -240,5 +249,14 @@ func zellerWeekday(y, m, d int) int {
 	C := int(y_ / 100)
 	G := 5*C + int(C/4)
 	Y := y_ % 100
-	return ((d + int(26*(m_+1)/10) + Y + int(Y/4) + G + 5) % 7) + 1
+	return DayOfWeek(((d + int(26*(m_+1)/10) + Y + int(Y/4) + G + 5) % 7) + 1)
+}
+
+func multiplyDigits(n int) int {
+	product := 1
+	for n > 0 {
+		product *= n % 10
+		n = n / 10
+	}
+	return product
 }
--- utils_test.go
+++ utils_test.go
@@ -146,6 +146,28 @@ func Test_Pow10(t *testing.T) {
 	}
 }
 
+func Test_ZellerWeekday(t *testing.T) {
+	cases := []struct {
+		InputY   int
+		InputM   int
+		InputD   int
+		Expected DayOfWeek
+	}{
+		// 異常系: グレゴリオ歴以前
+		{1582, 10, 14, None},
+		{1580, 1, 1, None},
+		// 正常系
+		{1582, 10, 15, Friday},
+		{1904, 1, 4, Monday},
+		{1904, 4, 5, Tuesday},
+	}
+	for _, tc := range cases {
+		if actual := ZellerWeekday(tc.InputY, tc.InputM, tc.InputD); actual != tc.Expected {
+			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
+		}
+	}
+}
+
 func Test_multiplyDigits(t *testing.T) {
 	cases := []struct {
 		Input0   int
@@ -156,23 +178,6 @@ func Test_multiplyDigits(t *testing.T) {
 	}
 	for _, tc := range cases {
 		if actual := multiplyDigits(tc.Input0); actual != tc.Expected {
-			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
-		}
-	}
-}
-
-func Test_zellerWeekday(t *testing.T) {
-	cases := []struct {
-		InputY   int
-		InputM   int
-		InputD   int
-		Expected int
-	}{
-		{1904, 1, 4, 1},
-		{1904, 4, 5, 2},
-	}
-	for _, tc := range cases {
-		if actual := zellerWeekday(tc.InputY, tc.InputM, tc.InputD); actual != tc.Expected {
 			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
 		}
 	}
