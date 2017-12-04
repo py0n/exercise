d0abf2c95bd9e80a2a44f5ac8a0222c2178c67d2
Pyon <py0n@users.noreply.github.com>
2017-12-04 17:09:30 +0900

    problem 19

--- pe0019_test.go
+++ pe0019_test.go
@@ -3,14 +3,17 @@ package project_euler
 import "testing"
 
 var pe0019Cases = []struct {
+	Input0    int
+	Input1    int
 	Expeceted int
 }{
-	{5200},
+	{1904, 1904, 1},
+	{1901, 2000, 171},
 }
 
 func Test_PE0019(t *testing.T) {
 	for _, tc := range pe0019Cases {
-		if actual := PE0019(); actual != tc.Expeceted {
+		if actual := PE0019(tc.Input0, tc.Input1); actual != tc.Expeceted {
 			t.Errorf("expected=%v, actual=%v", tc.Expeceted, actual)
 		}
 	}
