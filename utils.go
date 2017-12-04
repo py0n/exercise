79d4dfb201dcc789157a03aa103bf83c53ce24b7
Pyon <py0n@users.noreply.github.com>
2017-12-05 00:54:30 +0900

    入力が不正の際の処理を追加

--- utils.go
+++ utils.go
@@ -202,7 +202,9 @@ func IsPalindrome(s string) bool {
 // Lcm 最小公倍數を計算する
 func Lcm(n, m int) int {
 	g := Gcm(n, m)
-	if g == 1 {
+	if g == 0 {
+		return 0
+	} else if g == 1 {
 		return n * m
 	}
 	return n * m / g
