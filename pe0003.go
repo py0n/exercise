e14659f440f47be090393ba3c0ab813e37b57a9b
Pyon <py0n@users.noreply.github.com>
2017-12-05 16:43:33 +0900

    sort.Slice (sort.SliceStable) を使った方法も追加

--- pe0003.go
+++ pe0003.go
@@ -7,7 +7,7 @@ import (
 )
 
 // https://projecteuler.net/problem=3
-func PE0003(n int) (int, error) {
+func PE0003SortSort(n int) (int, error) {
 	primeNumbers := []int{}
 
 	uLimit := int(math.Sqrt(float64(n)))
@@ -22,6 +22,31 @@ func PE0003(n int) (int, error) {
 	}
 
 	sort.Sort(sort.Reverse(sort.IntSlice(primeNumbers)))
+
+	for _, p := range primeNumbers {
+		if n%p == 0 {
+			return p, nil
+		}
+	}
+
+	return 0, errors.New("no solution")
+}
+
+func PE0003SortSlice(n int) (int, error) {
+	primeNumbers := []int{}
+
+	uLimit := int(math.Sqrt(float64(n)))
+
+	primeGenerator := NewPrimeGenerator()
+	for {
+		m := primeGenerator.Next()
+		if m > uLimit {
+			break
+		}
+		primeNumbers = append(primeNumbers, m)
+	}
+
+	sort.SliceStable(primeNumbers, func(i, j int) bool { return j < i })
 
 	for _, p := range primeNumbers {
 		if n%p == 0 {
--- pe0017_test.go
+++ pe0017_test.go
@@ -2,19 +2,39 @@ package project_euler
 
 import "testing"
 
-func Test_PE0017(t *testing.T) {
-	cases := []struct {
-		Input    int
-		Expected int
-	}{
-		{1000, 21124},
-	}
-	for _, tc := range cases {
-		actual, err := PE0017(tc.Input)
+var pe0017Cases = []struct {
+	Input    int
+	Expected int
+}{
+	{1000, 21124},
+}
+
+func Test_PE0017SortSort(t *testing.T) {
+	for _, tc := range pe0017Cases {
+		actual, err := PE0017SortSort(tc.Input)
 		if err != nil {
 			t.Fatal(err)
 		} else if actual != tc.Expected {
 			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
 		}
 	}
+}
+
+func Benchmark_PE0017SortSort(b *testing.B) {
+	PE0017SortSort(b.N)
+}
+
+func Test_PE0017SortSlice(t *testing.T) {
+	for _, tc := range pe0017Cases {
+		actual, err := PE0017SortSort(tc.Input)
+		if err != nil {
+			t.Fatal(err)
+		} else if actual != tc.Expected {
+			t.Errorf("expected=%v, actual=%v", tc.Expected, actual)
+		}
+	}
+}
+
+func Benchmark_PE0017SortSlice(b *testing.B) {
+	PE0017SortSlice(b.N)
 }
