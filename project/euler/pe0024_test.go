ae4eb75d5ab22c52a6e0a487249f628a6f4ef5b3
Pyon <py0n@users.noreply.github.com>
2017-12-08 11:06:00 +0900

    problem 24

--- project/euler/pe0024_test.go
+++ project/euler/pe0024_test.go
@@ -2,6 +2,22 @@ package euler
 
 import "testing"
 
+var pe0024Cases = []struct {
+	Input    int
+	Expected string
+}{
+	{1000000, "2783915460"},
+}
+
 func TestPE0024(t *testing.T) {
-	t.Fail()
+	for _, tc := range pe0024Cases {
+		if actual := PE0024(tc.Input); actual != tc.Expected {
+			t.Errorf(
+				"Input:%v\nExpected:%v\nActual:%v",
+				tc.Input,
+				tc.Expected,
+				actual,
+			)
+		}
+	}
 }
