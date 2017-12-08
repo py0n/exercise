42601d04e0f25eedce024b71b40b9d860f9e505a
Pyon <py0n@users.noreply.github.com>
2017-12-09 01:07:09 +0900

    Fibonacci數を計算する凾數

--- project/euler/utils.go
+++ project/euler/utils.go
@@ -191,6 +191,24 @@ func CollatzLength(n int) int {
 	return l + 1
 }
 
+// Fibonacci n番目のFibonacci數を計算
+func Fibonacci(nth int, fibonacciMap map[int]*big.Int) *big.Int {
+	if nth == 0 || nth == 1 {
+		return big.NewInt(int64(1))
+	}
+	if fibonacciMap == nil {
+		fibonacciMap = make(map[int]*big.Int)
+	}
+	if _, ok := fibonacciMap[nth]; ok {
+		return fibonacciMap[nth]
+	}
+	fibonacciMap[nth] = big.NewInt(int64(0)).Add(
+		Fibonacci(nth-1, fibonacciMap),
+		Fibonacci(nth-2, fibonacciMap),
+	)
+	return fibonacciMap[nth]
+}
+
 // Gcm 最大公約數を計算する
 func Gcm(n, m int) int {
 	if n < 1 || m < 1 {
