3e6e8c4291a557498986ff843fb475299df8d25b
Pyon <py0n@users.noreply.github.com>
2017-12-10 12:07:42 +0900

    コメントを追加

--- project/euler/pe0001.go
+++ project/euler/pe0001.go
@@ -1,6 +1,8 @@
 package euler
 
 // https://projecteuler.net/problem=1
+
+// PE0001 1000以下の3と5の倍數の總和を計算
 func PE0001(n int) int {
 	result := 0
 	for i := 0; i < n; i++ {
--- project/euler/pe0002.go
+++ project/euler/pe0002.go
@@ -5,6 +5,8 @@ import (
 )
 
 // https://projecteuler.net/problem=2
+
+// PE0002 4,000,000を越えないFibonacci數の内、偶數である物の總和を計算
 func PE0002(n int) (int, error) {
 	if n < 3 {
 		return 0, errors.New("n must be >= 3")
