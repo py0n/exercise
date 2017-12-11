6fd98940c471fbaafd258b3919296f1e10d116ad
Pyon <py0n@users.noreply.github.com>
2017-12-11 20:35:59 +0900

    Project Euler投稿用

--- project/euler/pe0026_test.go
+++ project/euler/pe0026_test.go
@@ -21,3 +21,37 @@ func TestPE0026(t *testing.T) {
 		}
 	}
 }
+
+/*
+Project Euler投稿用
+func TestPE0026b(t *testing.T) {
+
+	recurringCycleLength := func(n int) int {
+		qs := []int{}
+		rs := []int{}
+		for q, r := 0, 10; r > 0; q, r = r/n, (r%n)*10 {
+			for i := 0; i < len(qs); i++ {
+				if qs[i] != q || rs[i] != r {
+					continue
+				}
+				return len(qs[i:])
+			}
+			qs = append(qs, q)
+			rs = append(rs, r)
+		}
+		return 0
+	}
+
+	result, maxLength := 0, 0
+
+	for n := 1; n < 1000; n++ {
+		if l := recurringCycleLength(n); l > maxLength {
+			result, maxLength = n, l
+		}
+	}
+
+	if result != 983 {
+		t.Errorf("Expected:%v, Result:%v", 983, result)
+	}
+}
+*/
