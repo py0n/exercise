9f0c1efe04cd4f1557f551685ff5066f3dc5cfd1
Pyon <py0n@users.noreply.github.com>
2017-12-12 21:02:44 +0900

    Fibonacci關係を分離

--- project/euler/utils_test.go
+++ project/euler/utils_test.go
@@ -27,36 +27,6 @@ func TestBigNumberLength(t *testing.T) {
 	}
 }
 
-func TestFibonacci(t *testing.T) {
-	cases := []struct {
-		Input    int
-		Expected *big.Int
-	}{
-		{0, big.NewInt(1)},
-		{1, big.NewInt(1)},
-		{2, big.NewInt(2)},
-		{3, big.NewInt(3)},
-		{4, big.NewInt(5)},
-		{5, big.NewInt(8)},
-		{6, big.NewInt(13)},
-		{7, big.NewInt(21)},
-		{8, big.NewInt(34)},
-		{9, big.NewInt(55)},
-		{10, big.NewInt(89)},
-		{11, big.NewInt(144)},
-	}
-	for _, tc := range cases {
-		if actual := Fibonacci(tc.Input, nil); actual.Cmp(tc.Expected) != 0 {
-			t.Errorf(
-				"Input:%v\nExpected:%v\nActual:%v",
-				tc.Input,
-				tc.Expected,
-				actual,
-			)
-		}
-	}
-}
-
 func TestGcm(t *testing.T) {
 	cases := []struct {
 		InputX   int
