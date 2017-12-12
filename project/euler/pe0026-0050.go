efdada00972c7fa72bc98cac25de9df360424c91
Pyon <py0n@users.noreply.github.com>
2017-12-12 18:57:31 +0900

    26-50をひとまとめ

--- project/euler/pe0026-0050.go
+++ project/euler/pe0026-0050.go
@@ -1,5 +1,22 @@
 package euler
 
+// PE0026 d<nの内、1/dの循環節が最も長いものを計算する
+//
+// https://projecteuler.net/problem=26
+func PE0026(n int) int {
+	result, maxLength := 0, 0
+	for i := 1; i < n; i++ {
+		if l := CalculateRecurringCycleLength(i); l > maxLength {
+			maxLength = l
+			result = i
+		}
+	}
+	return result
+}
+
+// 循環少數は面白い
+// http://www2.math.kindai.ac.jp/~chinen/junkan_f/junkan.html
+
 // PE0027a 素数生成式
 //
 // |a| < 1000, |b| < 1000 の時, n^2+an+b で n=0 から初めて連続する整数で素数を生成したときに,
@@ -7,6 +24,8 @@ package euler
 //
 // a, b の条件を考える
 //     1. n=0 の時, 0^2+a*0+b=b なので, b は素数
+//
+// https://projecteuler.net/problem=27
 func PE0027a(aMax, bMax int) int {
 	// 1000未満の素数を生成
 	pg := NewPrimeGenerator()
