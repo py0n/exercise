4bae261cf6bd522327a553bcd1c456c0f026e1cb
Pyon <py0n@users.noreply.github.com>
2017-12-26 02:16:20 +0900

    結果を格納するスライスの大きさを豫め求める

--- project/euler/permutation.go
+++ project/euler/permutation.go
@@ -2,7 +2,15 @@ package euler
 
 // Permutation 文字列の順列を生成
 func Permutation(s string, k int) []string {
-	ss := []string{} // 結果を格納
+	rs := []rune(s)
+
+	// 引數檢査
+	if k < 1 || len(rs) < k {
+		return nil
+	}
+
+	// 結果を格納する配列
+	ss := make([]string, 0, PermutationNumber(len(rs), k))
 
 	var perm func([]rune, int)
 
@@ -20,7 +28,53 @@ func Permutation(s string, k int) []string {
 		}
 	}
 
-	perm([]rune(s), 0)
+	perm(rs, 0)
 
 	return ss
+}
+
+// PermutationA 文字列の順列を生成
+func PermutationA(s string, k int) []string {
+	rs := []rune(s)
+
+	// 引數檢査
+	if k < 1 || len(rs) < k {
+		return nil
+	}
+
+	// 結果を格納する配列
+	ss := make([]string, 0, PermutationNumber(len(rs), k))
+
+	var perm func([]rune, int)
+
+	perm = func(rs []rune, i int) {
+		if i == k {
+			ss = append(ss, string(rs[:i]))
+		} else {
+			rt := make([]rune, len(rs))
+			copy(rt, rs)
+			perm(rt, i+1)
+			for j := i + 1; j < len(rs); j++ {
+				rt[i], rt[j] = rt[j], rt[i]
+				perm(rt, i+1)
+			}
+		}
+	}
+
+	perm(rs, 0)
+
+	return ss
+}
+
+// PermutationNumber 順列の數P(m,n)
+func PermutationNumber(m, n int) int {
+	if m < 1 || n < 1 || m < n {
+		return 0
+	}
+
+	p := 1
+	for i := m - n + 1; i <= m; i++ {
+		p *= i
+	}
+	return p
 }
--- project/euler/permutation_test.go
+++ project/euler/permutation_test.go
@@ -1,5 +1,7 @@
 package euler
 
+// 順列生成のテスト
+
 import (
 	"reflect"
 	"testing"
@@ -27,6 +29,85 @@ func TestPermutation(t *testing.T) {
 				"InputString:%v\nInputK:%v\nExpected:%v\nActual:%v",
 				tc.InputString,
 				tc.InputK,
+				tc.Expected,
+				actual,
+			)
+		}
+	}
+}
+
+func BenchmarkPermutation(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Permutation("123456789", 9)
+	}
+}
+
+func TestPermutationA(t *testing.T) {
+	cases := []struct {
+		InputString string
+		InputK      int
+		Expected    []string
+	}{
+		{"abc", 2, []string{"ab", "ac", "ba", "bc", "ca", "cb"}},
+		{"abc", 3, []string{"abc", "acb", "bac", "bca", "cab", "cba"}},
+		{"abcd", 2, []string{
+			"ab", "ac", "ad",
+			"ba", "bc", "bd",
+			"ca", "cb", "cd",
+			"da", "db", "dc",
+		}},
+		{"abcd", 3, []string{
+			"abc", "abd", "acb", "acd", "adb", "adc",
+			"bac", "bad", "bca", "bcd", "bda", "bdc",
+			"cab", "cad", "cba", "cbd", "cda", "cdb",
+			"dab", "dac", "dba", "dbc", "dca", "dcb",
+		}},
+		// 異常系
+		{"abc", 0, nil},  // K<1
+		{"abc", -1, nil}, // K<1
+		{"abc", 4, nil},  // K>len(String)
+		{"abcd", 5, nil}, // K>len(String)
+	}
+	for _, tc := range cases {
+		if actual := PermutationA(tc.InputString, tc.InputK); !reflect.DeepEqual(actual, tc.Expected) {
+			t.Errorf(
+				"InputString:%v\nInputK:%v\nExpected:%v\nActual:%v",
+				tc.InputString,
+				tc.InputK,
+				tc.Expected,
+				actual,
+			)
+		}
+	}
+}
+
+func BenchmarkPermutationA(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PermutationA("123456789", 9)
+	}
+}
+
+func TestPermutationNumber(t *testing.T) {
+	cases := []struct {
+		InputM   int
+		InputN   int
+		Expected int
+	}{
+		{1, 1, 1},
+		{10, 1, 10},
+		{10, 4, 10 * 9 * 8 * 7},
+		{10, 10, 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1},
+		// 異常系
+		{0, 2, 0}, // m < 1
+		{2, 0, 0}, // n < 1
+		{2, 3, 0}, // m < n
+	}
+	for _, tc := range cases {
+		if actual := PermutationNumber(tc.InputM, tc.InputN); actual != tc.Expected {
+			t.Errorf(
+				"InputM:%v\nInputN:%v\nExpected:%v\nActual:%v",
+				tc.InputM,
+				tc.InputN,
 				tc.Expected,
 				actual,
 			)
