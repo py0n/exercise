37d1a0bedbdd3da26d66ab5b865d1871f338ce6e
Pyon <py0n@users.noreply.github.com>
2017-12-25 23:49:48 +0900

    引數を減らす

--- project/euler/permutation.go
+++ project/euler/permutation.go
@@ -4,23 +4,23 @@ package euler
 func Permutation(s string, k int) []string {
 	ss := []string{} // 結果を格納
 
-	var perm func([]rune, int, int)
+	var perm func([]rune, int)
 
-	perm = func(rs []rune, i, k int) {
-		if k == 0 {
+	perm = func(rs []rune, i int) {
+		if i == k {
 			ss = append(ss, string(rs[:i]))
 		} else {
 			rt := make([]rune, len(rs))
 			copy(rt, rs)
-			perm(rt, i+1, k-1)
+			perm(rt, i+1)
 			for j := i + 1; j < len(rs); j++ {
 				rt[i], rt[j] = rt[j], rt[i]
-				perm(rt, i+1, k-1)
+				perm(rt, i+1)
 			}
 		}
 	}
 
-	perm([]rune(s), 0, k)
+	perm([]rune(s), 0)
 
 	return ss
 }
