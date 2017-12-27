01a684740e8f8769181f2f8106be2affcfda1881
Pyon <py0n@users.noreply.github.com>
2017-12-27 14:16:42 +0900

    少し早くなつた。
    ```
    BenchmarkPermutation-4                20          72534000 ns/op 41543696 B/op     986412 allocs/op
    BenchmarkPermutationA-4               20          63793200 ns/op 50257266 B/op    1609940 allocs/op
    PASS
    ok      github.com/py0n/exercise/project/euler  2.901s
    ```

--- project/euler/permutation.go
+++ project/euler/permutation.go
@@ -34,36 +34,39 @@ func Permutation(s string, k int) []string {
 }
 
 // PermutationA 文字列の順列を生成
-func PermutationA(s string, k int) []string {
-	rs := []rune(s)
-
+func PermutationA(rs []rune, k int) [][]rune {
 	// 引數檢査
 	if k < 1 || len(rs) < k {
 		return nil
 	}
 
 	// 結果を格納する配列
-	ss := make([]string, 0, PermutationNumber(len(rs), k))
+	rss := make([][]rune, 0, PermutationNumber(len(rs), k))
 
-	var perm func([]rune, int)
+	var perm func([]rune, []rune, int)
 
-	perm = func(rs []rune, i int) {
-		if i == k {
-			ss = append(ss, string(rs[:i]))
-		} else {
-			rt := make([]rune, len(rs))
-			copy(rt, rs)
-			perm(rt, i+1)
-			for j := i + 1; j < len(rs); j++ {
-				rt[i], rt[j] = rt[j], rt[i]
-				perm(rt, i+1)
+	perm = func(sequence, rest []rune, i int) {
+		for j := 0; j < len(rest); j++ {
+			rest[j], rest[0] = rest[0], rest[j]
+
+			sequenceNew := make([]rune, i+1)
+			copy(sequenceNew, sequence)
+			sequenceNew[i] = rest[0]
+
+			restNew := make([]rune, len(rest)-1)
+			copy(restNew, rest[1:])
+
+			if i == k-1 {
+				rss = append(rss, sequenceNew)
+			} else {
+				perm(sequenceNew, restNew, i+1)
 			}
 		}
 	}
 
-	perm(rs, 0)
+	perm(nil, rs, 0)
 
-	return ss
+	return rss
 }
 
 // PermutationNumber 順列の數P(m,n)
--- project/euler/permutation_test.go
+++ project/euler/permutation_test.go
@@ -8,23 +8,22 @@ import (
 )
 
 func TestPermutation(t *testing.T) {
-	cases := []struct {
-		InputString string
-		InputK      int
-		Expected    []string
-	}{
-		{"abc", 2, []string{"ab", "ac", "ba", "bc", "ca", "cb"}},
-		{"abc", 3, []string{"abc", "acb", "bac", "bca", "cab", "cba"}},
-		{"abcd", 2, []string{"ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"}},
-		{"abcd", 3, []string{
-			"abc", "abd", "acb", "acd", "adb", "adc",
-			"bac", "bad", "bca", "bcd", "bda", "bdc",
-			"cab", "cad", "cba", "cbd", "cda", "cdb",
-			"dab", "dac", "dba", "dbc", "dca", "dcb",
-		}},
-	}
-	for _, tc := range cases {
+	for _, tc := range testPermutationCases {
 		if actual := Permutation(tc.InputString, tc.InputK); !reflect.DeepEqual(actual, tc.Expected) {
+			t.Errorf(
+				"InputString:%v\nInputK:%v\nExpected:%v\nActual:%v",
+				tc.InputString,
+				tc.InputK,
+				tc.Expected,
+				actual,
+			)
+		}
+	}
+}
+
+func TestPermutationA(t *testing.T) {
+	for _, tc := range testPermutationCases2 {
+		if actual := PermutationA(tc.InputString, tc.InputK); !reflect.DeepEqual(actual, tc.Expected) {
 			t.Errorf(
 				"InputString:%v\nInputK:%v\nExpected:%v\nActual:%v",
 				tc.InputString,
@@ -42,67 +41,14 @@ func BenchmarkPermutation(b *testing.B) {
 	}
 }
 
-func TestPermutationA(t *testing.T) {
-	cases := []struct {
-		InputString string
-		InputK      int
-		Expected    []string
-	}{
-		{"abc", 2, []string{"ab", "ac", "ba", "bc", "ca", "cb"}},
-		{"abc", 3, []string{"abc", "acb", "bac", "bca", "cab", "cba"}},
-		{"abcd", 2, []string{
-			"ab", "ac", "ad",
-			"ba", "bc", "bd",
-			"ca", "cb", "cd",
-			"da", "db", "dc",
-		}},
-		{"abcd", 3, []string{
-			"abc", "abd", "acb", "acd", "adb", "adc",
-			"bac", "bad", "bca", "bcd", "bda", "bdc",
-			"cab", "cad", "cba", "cbd", "cda", "cdb",
-			"dab", "dac", "dba", "dbc", "dca", "dcb",
-		}},
-		// 異常系
-		{"abc", 0, nil},  // K<1
-		{"abc", -1, nil}, // K<1
-		{"abc", 4, nil},  // K>len(String)
-		{"abcd", 5, nil}, // K>len(String)
-	}
-	for _, tc := range cases {
-		if actual := PermutationA(tc.InputString, tc.InputK); !reflect.DeepEqual(actual, tc.Expected) {
-			t.Errorf(
-				"InputString:%v\nInputK:%v\nExpected:%v\nActual:%v",
-				tc.InputString,
-				tc.InputK,
-				tc.Expected,
-				actual,
-			)
-		}
-	}
-}
-
 func BenchmarkPermutationA(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		PermutationA("123456789", 9)
+		PermutationA([]rune("123456789"), 9)
 	}
 }
 
 func TestPermutationNumber(t *testing.T) {
-	cases := []struct {
-		InputM   int
-		InputN   int
-		Expected int
-	}{
-		{1, 1, 1},
-		{10, 1, 10},
-		{10, 4, 10 * 9 * 8 * 7},
-		{10, 10, 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1},
-		// 異常系
-		{0, 2, 0}, // m < 1
-		{2, 0, 0}, // n < 1
-		{2, 3, 0}, // m < n
-	}
-	for _, tc := range cases {
+	for _, tc := range testPermutationNumberCases {
 		if actual := PermutationNumber(tc.InputM, tc.InputN); actual != tc.Expected {
 			t.Errorf(
 				"InputM:%v\nInputN:%v\nExpected:%v\nActual:%v",
@@ -113,4 +59,87 @@ func TestPermutationNumber(t *testing.T) {
 			)
 		}
 	}
+}
+
+var testPermutationCases = []struct {
+	InputString string
+	InputK      int
+	Expected    []string
+}{
+	{"abc", 2, []string{
+		"ab", "ac",
+		"ba", "bc",
+		"ca", "cb",
+	}},
+	{"abc", 3, []string{
+		"abc", "acb",
+		"bac", "bca",
+		"cab", "cba",
+	}},
+	{"abcd", 2, []string{
+		"ab", "ac", "ad",
+		"ba", "bc", "bd",
+		"ca", "cb", "cd",
+		"da", "db", "dc",
+	}},
+	{"abcd", 3, []string{
+		"abc", "abd", "acb", "acd", "adb", "adc",
+		"bac", "bad", "bca", "bcd", "bda", "bdc",
+		"cab", "cad", "cba", "cbd", "cda", "cdb",
+		"dab", "dac", "dba", "dbc", "dca", "dcb",
+	}},
+	// 異常系
+	{"abc", 0, nil},  // K<1
+	{"abc", -1, nil}, // K<1
+	{"abc", 4, nil},  // K>len(String)
+	{"abcd", 5, nil}, // K>len(String)
+}
+
+var testPermutationCases2 = []struct {
+	InputString []rune
+	InputK      int
+	Expected    [][]rune
+}{
+	{[]rune("abc"), 2, [][]rune{
+		[]rune("ab"), []rune("ac"),
+		[]rune("ba"), []rune("bc"),
+		[]rune("ca"), []rune("cb"),
+	}},
+	{[]rune("abc"), 3, [][]rune{
+		[]rune("abc"), []rune("acb"),
+		[]rune("bac"), []rune("bca"),
+		[]rune("cab"), []rune("cba"),
+	}},
+	{[]rune("abcd"), 2, [][]rune{
+		[]rune("ab"), []rune("ac"), []rune("ad"),
+		[]rune("ba"), []rune("bc"), []rune("bd"),
+		[]rune("ca"), []rune("cb"), []rune("cd"),
+		[]rune("da"), []rune("db"), []rune("dc"),
+	}},
+	{[]rune("abcd"), 3, [][]rune{
+		[]rune("abc"), []rune("abd"), []rune("acb"), []rune("acd"), []rune("adb"), []rune("adc"),
+		[]rune("bac"), []rune("bad"), []rune("bca"), []rune("bcd"), []rune("bda"), []rune("bdc"),
+		[]rune("cab"), []rune("cad"), []rune("cba"), []rune("cbd"), []rune("cda"), []rune("cdb"),
+		[]rune("dab"), []rune("dac"), []rune("dba"), []rune("dbc"), []rune("dca"), []rune("dcb"),
+	}},
+	// 異常系
+	{[]rune("abc"), 0, nil},  // K<1
+	{[]rune("abc"), -1, nil}, // K<1
+	{[]rune("abc"), 4, nil},  // K>len(String)
+	{[]rune("abcd"), 5, nil}, // K>len(String)
+}
+
+var testPermutationNumberCases = []struct {
+	InputM   int
+	InputN   int
+	Expected int
+}{
+	{1, 1, 1},
+	{10, 1, 10},
+	{10, 4, 10 * 9 * 8 * 7},
+	{10, 10, 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1},
+	// 異常系
+	{0, 2, 0}, // m < 1
+	{2, 0, 0}, // n < 1
+	{2, 3, 0}, // m < n
 }
